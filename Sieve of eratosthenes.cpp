28f7dc12ef7f1e196298d72cb3900d307859b8ad
Tushar Jaiswal <54629298+tusharjaiswal123@users.noreply.github.com>
2021-02-03 20:23:19 +0530

    Create Sieve of eratosthenes.cpp

---
+++ Sieve of eratosthenes.cpp
@@ -0,0 +1,37 @@
+vector<ll> primes;
+vector<bool> prime;
+void Sieve(ll n)
+{
+	prime.assign(n + 5, 1);
+	prime[1] = false;
+	for (ll i = 2; i * i<= n; i++)
+	{
+		if (prime[i])
+		{
+			primes.pb(i);
+			for (ll j = i * i; j <= n; j += i)
+			{
+				prime[j] = false;
+			}
+		}
+	}
+}
+
+int main() {
+	fastio;
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	ll t,N;
+	t = 1;
+  N=1e5;
+  sieve(N);
+	cin >> t;
+	while (t--)
+	{
+		solve();
+	}
+	return 0;
+}
