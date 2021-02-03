8f4ca15c4e012e63e5087c6317c251af100ef2d0
Tushar Jaiswal <54629298+tusharjaiswal123@users.noreply.github.com>
2021-02-03 21:35:18 +0530

    Create ncr(pow,invmod).cpp

---
+++ ncr(pow,invmod).cpp
@@ -0,0 +1,55 @@
+vector<ll> fact;
+void pre(ll n, ll m)
+{
+	fact.resize(n + 5);
+	fact[0] = 1;
+
+	for (ll i = 1; i <= n; i++)
+		fact[i] = (fact[i] % m * i % m) % m;
+}
+
+ll pow(ll a, ll n, ll m)
+{
+	ll ans = 1;
+	while (n)
+	{
+		if (n % 2)
+			ans = (ans % m * a % m);
+
+		a = (a % m * a % m) % m;
+		n = n / 2;
+	}
+	return ans;
+}
+
+ll modinv(ll n)
+{
+	return pow(n, mod - 2, mod);
+}
+
+ll ncr(ll n, ll r, ll m)
+{
+	if (r > n)
+		return 0;
+
+	return (fact[n] % m * modinv(fact[n - r]) % m * modinv(fact[r]) % m) % m;
+}
+
+int main() {
+	fastio;
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	ll t, N;
+	t = 1;
+	N = 1e5;
+	pre(N, mod);
+	cin >> t;
+	while (t--)
+	{
+		solve();
+	}
+	return 0;
+}
