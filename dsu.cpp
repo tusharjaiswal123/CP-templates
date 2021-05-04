779c70714fef56fa7130892c64710421e72621bb
Tushar Jaiswal <54629298+tusharjaiswal123@users.noreply.github.com>
2021-05-04 16:42:39 +0530

    Update dsu.cpp

--- dsu.cpp
+++ dsu.cpp
@@ -1,33 +1,41 @@
-ll par[300005];
-ll r[300005];
-ll ep[300005];
-ll ext[300005];
-
-ll get(ll x)
+struct dsu
 {
-	return par[x] = (par[x] == x ? x : get(par[x]));
-}
+	vll par;
+	vll r;
 
-ll find(ll x)
-{
-	if (par[x] == x)
-		return ep[x] - ext[x];
-
-	return (ep[x] - ext[x] + find(par[x]));
-}
-
-void unin(ll a, ll b)
-{
-	ll p1 = get(a);
-	ll p2 = get(b);
-
-	if (r[p1] > r[p2])
-		swap(p1, p2);
-
-	if (p1 != p2)
+	void init(ll n)
 	{
-		par[p1] = p2;
-		ext[p1] = ep[p2];
-		r[p2] += r[p1];
+		r.assign(n + 10, 1);
+		par.resize(n + 10);
+
+		for (ll i = 1; i <= n + 5; i++)
+		{
+			par[i] = i;
+		}
 	}
-}
+
+	ll get(ll x)
+	{
+		return par[x] = (par[x] == x ? x : get(par[x]));
+	}
+
+	bool merge(ll a, ll b)
+	{
+		ll p1 = get(a);
+		ll p2 = get(b);
+
+		if (p1 != p2)
+		{
+			if (r[p1] > r[p2])
+				swap(p1, p2);
+
+			par[p1] = p2;
+			r[p2] += r[p1];
+
+			return false;
+		}
+
+		return true;
+	}
+
+};
