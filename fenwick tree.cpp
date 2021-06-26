265ed1c7607ed6522b87be72552476c216ae9185
Tushar Jaiswal <54629298+tusharjaiswal123@users.noreply.github.com>
2021-06-26 15:33:26 +0530

    Update fenwick tree.cpp

--- fenwick tree.cpp
+++ fenwick tree.cpp
@@ -1,41 +1,80 @@
-struct FenwickTree {
+struct fenwick {
 	vector<ll> bit;  // binary indexed tree
 	ll n;
 
-	FenwickTree(ll n)
+	void init(ll size)
 	{
-		this->n = n;
-		bit.assign(n, 0);
-
-		//build(a);
+		n = size;
+		bit.assign(n+1, 0);
 	}
 
-	void build(vll &a)
+	void build(ll a[])
 	{
-		ll i;
-		for (i = 0; i < a.size(); i++)
+		for (ll i = 0; i < n; i++)
 			update(i, a[i]);
 	}
 
 
-	ll query(ll r)
+	ll query(ll x)
 	{
 		ll ans = 0;
-
-		for (; r >= 0; r = (r & (r + 1)) - 1)
-		{
-			ans += bit[r];
-		}
+		for (ll i=x; i > 0; i -= i&-i)
+			ans += bit[i];
 
 		return ans;
 	}
-
-	ll query(ll l, ll r) {
-		return query(r) - query(l - 1);
+	
+	// zero based indexing 
+	ll query(ll l, ll r)
+	{
+		return query(r+1) - query(l);
 	}
-
-	void update(ll idx, ll delta) {
-		for (; idx < n; idx = idx | (idx + 1))
-			bit[idx] += delta;
+	
+	// zeno based indexing
+	void update(ll x, ll v)
+	{	
+		x++;
+		for (ll i=x; i<=n; i += i&-i)
+			bit[i] += v;
 	}
+};
+
+
+
+Sum in 2D:
+
+struct fenwick{
+	
+    vector<vector<int>> bit;
+    int n, m;
+
+    void init(ll nn,ll mm)
+    {
+	 n = nn;
+	 m = mm;
+	 bit = vector<vector<int>> (nn+1,vector<int(mm+1,0));
+    }
+    
+    //zero based indexing
+    int sum(int x, int y)
+    {	
+	x++;
+	y++;
+        int ans = 0;
+        for (int i = x; i >= 0; i -= i & -i)
+            for (int j = y; j >= 0; j -= j & -j)
+                ret += bit[i][j];
+	    
+        return ans;
+    }
+    
+    //zero based indexing
+    void add(int x, int y, int v)
+    {	
+	x++;
+	y++;
+        for (int i = x; i <= n; i += i & -i )
+            for (int j = y; j <= m; j += j & -j)
+                bit[i][j] += v;
+    }
 };
