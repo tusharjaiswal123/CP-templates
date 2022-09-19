08e6af1a033d6e50d4078b7564e47c5ee9fa76a9
Tushar Jaiswal <54629298+tusharjaiswal123@users.noreply.github.com>
2022-09-19 17:45:08 +0530

    Update pbds.cpp

--- pbds.cpp
+++ pbds.cpp
@@ -1,3 +1,6 @@
+https://codeforces.com/blog/entry/11080
+
+
 #include<ext/pb_ds/assoc_container.hpp>
 #include<ext/pb_ds/tree_policy.hpp>
 
@@ -21,3 +24,5 @@ typedef tree<long long, null_type, less_equal<long long>, rb_tree_tag, tree_orde
 2. lower_bound works like upper_bound
 3. upper_bound works like lower bound
 4. any.erase(any.find_by_order(any.order_of_key(val))); // use this for deleting
+
+https://www.codechef.com/viewsolution/74496520
